f5dcd7905f6270711e1f0ca50ad157fc39866b53
dengcs <540390359@qq.com>
2018-12-13 20:32:42 +0800

    up

--- common/lualib/agent_pool.lua
+++ common/lualib/agent_pool.lua
@@ -1,48 +1,42 @@
 local skynet        = require "skynet"
 
+local tb_insert = table.insert
+local tb_remove = table.remove
+
 local max_count = 10
 local need_add = false
 local agent_pool = {}
 
 function agent_pool.new()
-    local pool = {capacity = 0,agent_queue = {}}
+    local pool = {agent_queue = {}}
 
     setmetatable(pool, {__index = agent_pool})
 
     return pool
 end
 
-function agent_pool:push(agent)
-    if not self.agent_queue[agent] then
-        self.agent_queue[agent] = agent
-        self.capacity = self.capacity + 1
-    end
-end
-
 function agent_pool:inc()
     skynet.fork(function()
-        if self.capacity >= max_count then
+        local capacity = #self.agent_queue
+        if capacity >= max_count then
             need_add = false
             return
         end
 
         local agent = skynet.newservice("agentd")
         skynet.call(agent, "lua", "init")
-        self:push(agent)
+        tb_insert(self.agent_queue, agent)
     end)
 end
 
 function agent_pool:pop()
-    if self.capacity > 0 then
+    local capacity = #self.agent_queue
+    if capacity > 0 then
         if need_add then
             self:inc()
         end
-        local agent,_ = next(self.agent_queue)
-        if agent then
-            self.agent_queue[agent] = nil
-            self.capacity = self.capacity - 1
-            return agent
-        end
+        local agent = tb_remove(self.agent_queue)
+        return agent
     else
         need_add = true
         self:inc()
