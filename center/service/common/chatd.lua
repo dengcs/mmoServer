57ad65bb71ae812fad0cee25bcdb5880c811fbb9
dengcs <540390359@qq.com>
2019-03-25 21:04:47 +0800

    up

--- center/service/common/chatd.lua
+++ center/service/common/chatd.lua
@@ -1,99 +1,394 @@
-local nova = require "nova"
-local service = require "service"
+---------------------------------------------------------------------
+--- 聊天系统服务
+---------------------------------------------------------------------
+local service  = require "factory.service"
+local skynet   = require "skynet"
 
-local CMD = {}
+---------------------------------------------------------------------
+--- 内部变量/内部逻辑
+---------------------------------------------------------------------
 
--- 禁言用户
-local ban_users = {}
-local dirty = false
+-- 在线用户列表
+local users = nil
+-- 频道数组
+local channels = nil
+--自定义频道 生产唯一cid，控制参数
+--频道两个参数：cid-频道唯一id，ctype-频道类型
+--cid为1-10频道，cid==ctype
+local __cid = 100
+-----------------------------------------------------------
+--- 队员数据模型
+-----------------------------------------------------------
+local User = class("user")
 
--- 加载禁言信息
-local function load_ban_data()
-    local _, data = nova.call(GAME.SERVICE.KVCACHED, "lua", "get", "kvcached.chat.ban")
-    if data ~= nil then
-        ban_users = data
+-- 构造模型实例
+-- 1. 角色数据 pid
+function User:ctor(pid)
+    self.pid    = pid
+    self.cids = {}
+end
+
+-- 登录时，刷新数据
+function User:update(pid)
+    if self.pid ~= pid then
+        LOG_ERROR("[chatd] User:update uid error! pid=%s,self.pid=%s",pid,self.pid)
+        self.pid = pid
     end
 end
 
--- 保存禁言信息
-local function save_ban_data()
-    nova.send(GAME.SERVICE.KVCACHED, "lua", "set", "kvcached.chat.ban", ban_users)
-end
-
--- 定时器间隔(秒)
-local interval = 60
-
--- 定时任务
-local function on_timer()
-    if dirty then
-        save_ban_data()
-        dirty = false
-    end
-    
-	-- 重置定时器
-	this.schedule(on_timer, interval, 1)
-end
-
-
-local function init_handler()
-    load_ban_data()
-    on_timer()
-end
-
-local function exit_handler()
-    this.unschedule_all()
-    
-    if dirty then
-        dirty = false
-        save_ban_data()
+-- 进入频道，专门供channel调用
+function User:enter(cid)
+    if self.cids[cid] then
+        -- LOG_DEBUG("[chatd] User:enter user already enter channel. pid=%s,cid=%s",self.pid,cid)
+    else
+        self.cids[cid] = cid
     end
 end
 
--- 新用户
-function CMD.send_msg_to_world(uid, content)
-    local now = os.time()
-    if ban_users[uid] then
-        if ban_users[uid].end_time then
-            if ban_users[uid].end_time > now then
-                LOG_DEBUG("uid[%s] is baned", uid)
-                return
-            else
-                ban_users[uid] = nil
+-- 退出频道，专门供channel调用
+function User:exit(cid)
+    if self.cids[cid] then
+        self.cids[cid] = nil
+    else
+        LOG_DEBUG("[chatd] User:exit user no in channel. pid=%s,cid=%s",self.pid,cid)
+    end
+end
+
+-- 下发通知消息
+-- 1. 消息名称
+-- 2. 消息内容
+function User:response(name, data)
+    this.usersend(self.pid, "response_message", name, data)
+end
+
+-- 通过类型获取频道，用户在一种频道上只能拥有一个
+-- 1. 频道类型
+function User:get_channel_by_type(ctype)
+    local channel, cur_channel
+    for _,cid in pairs(self.cids) do
+        channel = channels[cid]
+        if channel then
+            if channel.ctype == ctype then
+                assert(cur_channel == nil)
+                cur_channel = channel
             end
         else
-            LOG_DEBUG("uid[%s] is forever baned", uid)
-            return
+            self.cids[cid] = nil
         end
     end
-    nova.send(GAME.SERVICE.ONLINED, "lua", "broadcast", "chat_msg_notify", content)
+    return cur_channel
 end
 
--- 禁言用户
-function CMD.ban_user(uids, end_time, forever)
-    for _, v in pairs(uids) do
-        ban_users[v] = {end_time = end_time, forever = forever}
+-- 广播用户的加入的某种频道
+-- 1. 频道类型
+-- 2. 协议名
+-- 3.协议结构体
+function User:broadcast(ctype, name, data)
+    local cur_channel = nil
+    for _,cid in pairs(self.cids) do
+        if channels[cid] and channels[cid].ctype then
+            assert(cur_channel == nil)
+            cur_channel = channels[cid]
+        end
     end
-    dirty = true
+    if cur_channel then
+        cur_channel:broadcast(name, data)
+    end
 end
 
--- 服务注册
-
-local handler = {}
-
--- 消息分发逻辑
--- 1. 消息来源
--- 2. 消息类型
--- 3. 消息内容
-function handler.command_handler(source, cmd, ...)
-	local fn = CMD[cmd]
-	if fn then
-		return fn(...)
-	else
-		ERROR("svcmanager : command[%s] can't find!!!", cmd)
-	end
+--玩家登陆时，推送最近10条聊天记录
+function User:access_msgs()
+    local ctypes = {GAME.CHAT.CHANNEL.WORLD,GAME.CHAT.CHANNEL.CLUB}
+    local channel, chat_msgs
+    for _,ctype in ipairs(ctypes) do
+        -- LOG_DEBUG("User:access_msgs ctype=%s",ctype)
+        channel = self:get_channel_by_type(ctype)
+        if channel then
+            chat_msgs = channel:get_chat_msgs()
+            if chat_msgs then
+                -- LOG_DEBUG("User:access_msgs channel=%s #chat_msgs=%s",channel,#chat_msgs)
+                for _,chat_msg in ipairs(chat_msgs) do
+                    self:response(chat_msg.name, chat_msg.data)
+                end
+            end
+        end
+    end
 end
 
-handler.init_handler = init_handler
-handler.exit_handler = exit_handler
+-- function User:tostring()
+--     return "User:pid="..self.pid..",cids="..table.tostring(self.cids)
+-- end
 
-service.start(handler)
+-----------------------------------------------------------
+--- 频道数据模型
+-----------------------------------------------------------
+local Channel = class("channel")
+
+-- 构建实例
+-- 1.频道id
+function Channel:ctor(cid,ctype)
+    self.cid = assert(cid)
+    self.ctype = assert(ctype)
+    self.users = {}     -- 用户列表
+end
+
+function Channel:push_chat_msg(chat_msg)
+    self.chat_msgs = self.chat_msgs or {}
+    table.insert(self.chat_msgs,chat_msg)
+
+    local ChatMessageMaxOffline = this.sheetdata("ConfigBaseData", "ChatMessageMaxOffline", "Data")
+    -- LOG_DEBUG("Channel:push_chat_msg #self.chat_msgs=%s ChatMessageMaxOffline=%s",#self.chat_msgs,ChatMessageMaxOffline)
+    while #self.chat_msgs > ChatMessageMaxOffline do
+        table.remove(self.chat_msgs,1)
+    end
+end
+
+function Channel:get_chat_msgs()
+    return self.chat_msgs
+end
+
+--用户进入频道
+function Channel:enter_user(user)
+    -- if self.users[user.pid] then
+    --     LOG_DEBUG("[chatd] Channel:enter_user user is exist!!  user.pid=%s",user.pid)
+    -- end
+    self.users[user.pid] = user
+    user:enter(self.cid)
+end
+
+--用户退出频道
+function Channel:exit_user(pid)
+    local user = self.users[pid]
+    if user then
+        self.users[pid] = nil
+        user:exit(self.cid)
+        -- else
+        -- LOG_DEBUG("[chatd] Channel:exit_user user not in Channel!!  pid=%s",pid)
+    end
+    --除了世界频道，空频道都进行清理
+    if next(self.users) == nil then
+        --低于100固定频道不能删除
+        if self.cid > 100 then
+            channels[self.cid] = nil
+        end
+    end
+end
+
+--频道退出
+function Channel:on_exit()
+    for _,user in pairs(self.users) do
+        user:exit(self.cid)
+    end
+end
+
+-- 频道广播
+-- 1. 消息名称
+-- 2. 消息内容
+function Channel:broadcast(name, data)
+    --社团消息需要缓存10条（策划需求）
+    if self.ctype == GAME.CHAT.CHANNEL.CLUB then
+        self:push_chat_msg({name=name,data=data})
+    end
+    for pid in pairs(self.users) do
+        this.usersend(pid, "response_message", name, data)
+    end
+end
+
+-- 全服广播,只有全服频道才可使用世界广播
+-- 1. 消息名称
+-- 2. 消息内容
+function Channel:rbroadcast(name, data)
+    assert(GAME.CHAT.CHANNEL.SERVERS == self.ctype)
+    skynet.send(GLOBAL.SERVICE.USERCENTERD, "lua", "broadcast","response_message", name, data)
+end
+
+-- 世界广播,只有世界频道才可使用世界广播
+-- 1. 消息名称
+-- 2. 消息内容
+function Channel:wbroadcast(name, data)
+    assert(GAME.CHAT.CHANNEL.WORLD == self.ctype)
+    --世界消息需要缓存10条（策划需求）
+    self:push_chat_msg({name=name,data=data})
+    skynet.send(GLOBAL.SERVICE.USERCENTERD, "lua", "broadcast","response_message", name, data)
+end
+
+--打印信息，调试使用
+function Channel:tostring()
+    local logs = {}
+    for _,user in pairs(self.users) do
+        table.insert(logs,user:tostring())
+    end
+    local logs_txt = table.concat(logs,"\n")
+    return "Channel:cid="..self.cid..",users="..logs_txt
+end
+
+function __create_channel(ctype)
+    local cid = __cid
+    __cid = __cid+1
+    local channel = Channel.new(cid,ctype)
+    channels[channel.cid] = channel
+    return channel
+end
+
+---------------------------------------------------------------------
+--- 服务导出业务接口
+---------------------------------------------------------------------
+local command = {}
+
+-- 角色上线登录
+-- 1. 指令来源
+-- 2. 角色编号
+function command.on_login(source, pid)
+    assert(pid>0,"[chatd]command.on_login uid is invalid!")
+    local user = users[pid]
+    if not user then
+        user = User.new(pid)
+        users[pid] = user
+    end
+    channels[GAME.CHAT.CHANNEL.WORLD]:enter_user(user)
+end
+
+-- 角色断线
+-- 1. 指令来源
+-- 2. 角色编号
+function command.on_quit(source, pid)
+    local user = users[pid]
+    if not user then
+        return
+    end
+    assert(user.pid == pid,"[chatd]command.on_quit pid is invalid!")
+    users[pid] = nil
+    for _,cid in pairs(user.cids) do
+        if channels[cid] then
+            channels[cid]:exit_user(pid)
+        else
+            LOG_INFO("[chatd] command.on_quit channel not exist! cid=%s",cid)
+        end
+    end
+end
+
+--创建频道
+--1.频道类型
+--2.创建时需要加入读玩家
+function command.create_channel(source,ctype,pids)
+    local channel = __create_channel(ctype)
+    local user
+    for _,pid in ipairs(pids) do
+        user = users[pid]
+        if user then
+            channel:enter_user(user)
+        else
+            LOG_ERROR("[chatd] command.create_channel user never login server! pid=%s",pid)
+        end
+    end
+    return channel.cid
+end
+
+function command.remove_channel(source,cid)
+    local channel = channels[cid]
+    if channel then
+        channel:on_exit()
+    else
+        LOG_INFO("[chatd] command.remove_channel channel not exist! cid=%s",cid)
+    end
+end
+
+-- 给特定频道广播客户端聊天数据
+-- 1.serverid
+-- 2.发送指定的频道
+-- 3.proto名
+-- 4.proto
+-- 5.指定玩家的pid数组
+function command.broadcast(source,schannel,proto_name, proto, pids)
+    local ret = 0
+    --发送到全服频道
+    if schannel == GAME.CHAT.CHANNEL.SERVERS then
+        channels[GAME.CHAT.CHANNEL.SERVERS]:rbroadcast(proto_name,proto)
+
+        --发送到世界频道
+    elseif schannel == GAME.CHAT.CHANNEL.WORLD then
+        channels[GAME.CHAT.CHANNEL.WORLD]:wbroadcast(proto_name,proto)
+
+        --发送到当前game节点
+        -- elseif schannel == GAME.CHAT.CHANNEL.ROOM then
+        -- channels[GAME.CHAT.CHANNEL.WORLD]:broadcast(proto_name,proto)
+        --发送到私聊频道
+    elseif schannel == GAME.CHAT.CHANNEL.PRIVATE
+            or schannel == GAME.CHAT.CHANNEL.FRIEND then
+        local sender = users[tonumber(proto.send_pid)]
+        if sender then
+            for _,pid in ipairs(pids) do
+                this.usersend(pid, "response_message", proto_name, proto)
+            end
+        else
+            ret = ERRCODE.CHAT_PLAYER_OFFLINE
+        end
+    else
+        ret = ERRCODE.CHAT_CHANNEL_ERROR
+    end
+    return ret
+end
+
+--请求最近的聊天信息
+function command.access_msgs(source, pid)
+    -- LOG_DEBUG("command.access_msgs source, pid=%s",pid)
+    local user = users[pid]
+    if not user then
+        return ERRCODE.CHAT_PLAYER_OFFLINE
+    end
+    user:access_msgs(source)
+    return 0
+end
+
+---------------------------------------------------------------------
+--- 服务事件回调（底层事件通知）
+---------------------------------------------------------------------
+local server = {}
+
+-- 服务构造通知
+-- 1. 构造参数
+function server.on_init(config)
+    assert(not channels)
+    users = {}
+    channels = {
+        [GAME.CHAT.CHANNEL.SERVERS] = Channel.new(GAME.CHAT.CHANNEL.SERVERS,GAME.CHAT.CHANNEL.SERVERS),
+        [GAME.CHAT.CHANNEL.WORLD] = Channel.new(GAME.CHAT.CHANNEL.WORLD,GAME.CHAT.CHANNEL.WORLD)
+    }
+end
+
+-- 服务停止通知
+function server.on_stop()
+    assert(false, "chatd : no on_stop")
+end
+
+-- 服务退出通知
+function server.on_exit()
+    assert(false, "chatd : no on_exit")
+end
+
+-- 网络消息通知
+-- 1. 套接字
+-- 2. 消息内容
+function server.on_message(fd, message)
+    assert(false, "chatd : no on_message")
+end
+
+-- 业务指令调用
+-- 1. 指令来源
+-- 2. 指令名称
+-- 3. 执行参数
+function server.on_command(source, cmd, ...)
+    local fn = command[cmd]
+    if fn then
+        return fn(source, ...)
+    else
+        ERROR("chatd : command[%s] not found!!!", cmd)
+    end
+end
+
+-- 启动服务对象
+service.start(server)
+
+
+
+
--- center/service/common/friendd.lua
+++ center/service/common/friendd.lua
@@ -1,170 +1,470 @@
-local skynet =require "skynet"
-local friend_manager = require "friend.friend_manager"
+---------------------------------------------------------------------
+--- 社交服务
+---------------------------------------------------------------------
+local service   = require "factory.service"
+local skynet    = require "skynet"
+local database  = require "common.database"
 
-local service = require "service"
+local tinsert = table.insert
 
-local CMD = {}
+-----------------------------------------------------------
+--- 常量表
+-----------------------------------------------------------
 
-local friend_save_interval = skynet.getenv("friend_save_interval") or 60
+-- 最大申请数量
+local MAX_APPLICANTS_VALUE = 100
 
--- 管理器
-local friend_mgr
--- 定时任务id
-local time_id
+-- 最大好友数量
+local MAX_FRIENDS_VALUE    = 100
 
--- 社交保存数据操作
-local function save_data()
-    --LOG_DEBUG("friendd save data ......")
-    friend_mgr:save_data()
+-- 最大敌人数量
+local MAX_ENEMIES_VALUE    = 100
+
+-----------------------------------------------------------
+--- 内部变量/内部逻辑
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+--- 内部類
+-----------------------------------------------------------
+
+-- 社交数据模型
+local udata = class("udata")
+
+-- 构建模型实例
+-- 1. 角色编号
+function udata:ctor(pid)
+    self.dirty              = false                         -- 数据脏标记
+    self.pid                = pid                           -- 角色编号
+    self.friend             = { len = 0, list = {} }        -- 好友信息
+    self.enemy              = { len = 0, list = {} }        -- 敌人信息
+    self.applicant          = { len = 0, list = {} }        -- 申请信息（申请成为目标好友）
+    self.authorize          = { len = 0, list = {} }        -- 申请信息（目标申请等待批准）
+    self.friendly_send      = { len = 0, list = {} }        -- 友情贈送
+    self.friendly_recive    = { len = 0, list = {} }        -- 友情接收
+    self.group              = { len = 0, list = {} }        -- 群信息
+    self.subscribe          = { len = 0, list = {} }        -- 订阅通知
 end
 
--- 启动
-local function init_handler()
-    friend_mgr =friend_manager.new()
-    friend_mgr:start()
-    time_id = this.schedule(save_data, friend_save_interval , SCHEDULER.REPEAT_FOREVER)
+-- 序列化
+function udata:serialize()
+    return skynet.packstring(
+            {
+                friend          = self.friend,
+                enemy           = self.enemy,
+                applicant       = self.applicant,
+                authorize       = self.authorize,
+                friendly_send   = self.friendly_send,
+                friendly_recive = self.friendly_recive,
+                group           = self.group,
+                subscribe       = self.subscribe,
+            })
 end
 
-function CMD.join_handler(uid)
-    friend_mgr:load_friend_data(uid)
+-- 反序列化
+function udata:unserialize(v)
+    if v then
+        local vdata = skynet.unpack(v)
+        self.friend             = vdata.friend
+        self.enemy              = vdata.enemy
+        self.applicant          = vdata.applicant
+        self.authorize          = vdata.authorize
+        self.friendly_send      = vdata.friendly_send
+        self.friendly_recive    = vdata.friendly_recive
+        self.group              = vdata.group
+    end
 end
 
--- 新用户
-function CMD.new_user(uid)
-    --LOG_DEBUG("friendd.new_user .............")
-    friend_mgr:new_user(uid)
+-- 检查脏标记
+function udata:check_dirty()
+    if self.dirty then
+        return true
+    else
+        return false
+    end
 end
 
--- 添加好友
-function CMD.add_friend(uid, uids)
-    --LOG_DEBUG("friendd.add_friend .............")
-    friend_mgr:add_friend(uid, uids)
+-- 设置脏标记
+function udata:set_dirty()
+    self.dirty = true
+end
+
+-- 清理脏标记
+function udata:clear_dirty()
+    self.dirty = false
+end
+
+-- 好友判断
+function udata:is_friend(pid)
+    if self.friend.list[pid] then
+        return true
+    else
+        return false
+    end
+end
+
+-- 增加好友
+function udata:add_friend(pid)
+    if (self.friend.list[pid] == nil) then
+        self.friend.list[pid] = {ctime = this.time()}
+        self.friend.len = self.friend.len + 1
+        self:set_dirty()
+
+        local data = social.get_data("get_friend_data", pid)
+        if data then
+            this.usersend(self.pid, "response_message", "social_add_friend_notice", {data = data})
+        end
+
+        add_subscribe(pid, self.pid)
+    end
 end
 
 -- 删除好友
-function CMD.del_friend(uid, uids)
-    --LOG_DEBUG("friendd.del_friend .............")
-    friend_mgr:del_friend(uid, uids)
+function udata:del_friend(pid)
+    if (self.friend.list[pid] ~= nil) then
+        self.friend.list[pid] = nil
+        self.friend.len = self.friend.len - 1
+        self:set_dirty()
+
+        this.usersend(self.pid, "response_message", "social_del_friend_notice", {pid = pid})
+
+        del_subscribe(pid, self.pid)
+    end
 end
 
--- 同意好友
-function CMD.agree_friend(uid, uids)
-    --LOG_DEBUG("friendd.agree_friend .............")
-    friend_mgr:agree_friend(uid, uids)
+-- 敌人判断
+function udata:is_enemy(pid)
+    if self.enemy.list[pid] then
+        return true
+    else
+        return false
+    end
 end
 
--- 拒绝好友
-function CMD.refuse_friend(uid, uids)
-    --LOG_DEBUG("friendd.refuse_friend .............")
-    friend_mgr:refuse_friend(uid, uids)
+-- 增加敌人
+function udata:add_enemy(pid)
+    if (self.enemy.list[pid] == nil) then
+        self.enemy.list[pid] = {ctime = this.time()}
+        self.enemy.len = self.enemy.len + 1
+        self:set_dirty()
+
+        add_subscribe(pid, self.pid)
+    end
 end
 
--- 添加黑名单
-function CMD.add_blacklist(uid, uids)
-    --LOG_DEBUG("friendd.add_blacklist .............")
-    friend_mgr:add_blacklist(uid, uids)
+-- 删除敌人
+function udata:del_enemy(pid)
+    if (self.enemy.list[pid] ~= nil) then
+        self.enemy.list[pid] = nil
+        self.enemy.len = self.enemy.len - 1
+        self:set_dirty()
+
+        del_subscribe(pid, self.pid)
+    end
 end
 
--- 删除黑名单
-function CMD.del_blacklist(uid, uids)
-    --LOG_DEBUG("friendd.del_blacklist .............")
-    friend_mgr:del_blacklist(uid, uids)
+-- 申请判断
+function udata:has_applied(pid)
+    if self.applicant.list[pid] then
+        return true
+    else
+        return false
+    end
 end
 
--- 获取好友列表数据
-function CMD.get_friend_data(uid)
-    --LOG_DEBUG("friendd.get_friend_data .............")
-    return friend_mgr:get_friend_data(uid)
+-- 增加申请
+function udata:add_applicant(pid)
+    if (self.applicant.list[pid] == nil) then
+        self.applicant.list[pid] = 1
+        self.applicant.len = self.applicant.len + 1
+        self:set_dirty()
+    end
 end
 
--- 获取亲密的好友列表数据
-function CMD.get_cohesion_friend_data(uid)
-    return friend_mgr:get_cohesion_friend_data(uid)
+-- 删除申请
+function udata:del_applicant(pid)
+    if (self.applicant.list[pid] ~= nil) then
+        self.applicant.list[pid] = nil
+        self.applicant.len = self.applicant.len - 1
+        self:set_dirty()
+    end
 end
 
--- 获取超过特定亲密值的好友列表数据 包含 cohesion值的
-function CMD.get_friend_data_over_cohesion(uid, cohesion)
-    return friend_mgr:get_friend_data_over_cohesion(uid, cohesion)
+-- 增加申请（待批准申请）
+function udata:add_authorize(pid, message)
+    if (self.authorize.list[pid] == nil) then
+        local ctime = this.time()
+        self.authorize.list[pid] = {ctime = ctime, msg = message}
+        self.authorize.len = self.authorize.len + 1
+        self:set_dirty()
+
+        local data = social.get_data("get_friend_data", pid)
+        data.msg = message
+        data.ctime  = ctime
+        this.usersend(self.pid, "response_message", "social_authorize_notice", {data = data})
+    end
 end
 
---获取关联关系数据
-function CMD.get_relation_data(uid)
-    return friend_mgr:get_relation_data(uid)
+-- 删除申请（待批准申请）
+function udata:del_authorize(pid)
+    if (self.authorize.list[pid] ~= nil) then
+        self.authorize.list[pid] = nil
+        self.authorize.len = self.authorize.len - 1
+        self:set_dirty()
+    end
 end
 
--- 获取提示信息
-function CMD.get_friend_tip(uid)
-    --LOG_DEBUG("friendd.get_friend_tip ......." .. uid)
-    return friend_mgr:get_friend_tip(uid)
+-- 判断朋友是否已满
+function udata:friend_has_full()
+    if self.friend.len >= MAX_FRIENDS_VALUE then
+        return true
+    else
+        return false
+    end
 end
 
--- 更新tip
-function CMD.update_friend_tip(uid,tip_data)
-    friend_mgr:update_friend_tip(uid, tip_data)
+-- 判断敌人是否已满
+function udata:enemy_has_full()
+    if self.enemy.len >= MAX_ENEMIES_VALUE then
+        return true
+    else
+        return false
+    end
 end
 
--- 添加亲密度
-function CMD.add_cohesion(uids)
-    friend_mgr:add_cohesion(uids)
+-- 判断申请是否已满
+function udata:applicant_has_full()
+    if self.applicant.len >= MAX_APPLICANTS_VALUE then
+        return true
+    else
+        return false
+    end
 end
 
--- 从uid列表中获取配对亲密关系
-function CMD.get_cohesion_relation(uids)
-    return friend_mgr:get_cohesion_relation(uids)
+-- 判断申请是否已满（待批准申请）
+function udata:authorize_has_full()
+    if self.authorize.len >= MAX_APPLICANTS_VALUE then
+        return true
+    else
+        return false
+    end
 end
 
--- 从uid列表中获取配对好友关系
-function CMD.get_friend_relation(uids)
-    return friend_mgr:get_friend_relation(uids)
+-----------------------------------------------------------
+--- 社交数据缓存
+-----------------------------------------------------------
+local evbuilder = require "common.cache.evbuilder"
+local builder   = require "luamon.cachebuilder"
+
+-- 社交数据加载模型
+local loader = class("service.common.social.cache.loader", require("luamon.cache.loader"))
+
+-- 数据加载逻辑
+-- 1. 数据键值
+function loader:load(key)
+    local vdata = skynet.call(GLOBAL.SERVICE.DATACACHE, "lua", "get", "social", key)
+    local udata = udata.new(key)
+    if vdata then
+        udata:unserialize(vdata)
+    end
+    return udata
 end
 
-function CMD.get_online_friend_list(uid)
-    return friend_mgr:get_online_friend_list(uid)
+-- 数据移除逻辑
+-- 1. 数据键值
+-- 2. 数据内容
+local function remove(key, udata)
+    if udata then
+        if (udata:check_dirty()) then
+            skynet.send(GLOBAL.SERVICE.DATACACHE, "lua", "set", "social", key, udata:serialize())
+            udata:clear_dirty()
+        end
+    end
 end
 
--- 校验是否被屏蔽
--- @uid uid
--- @ch_uid 校验id
--- @need_friend 是否需要是好友
-function CMD.is_shielded(uid, ch_uid, need_friend)
-    friend_mgr:is_shielded(uid, ch_uid, need_friend)
+-- 构建缓存对象
+cache = builder.new():capacity(5000)
+               :evbuilder(evbuilder.new())
+               :loader(loader.new())
+               :removal(remove)
+               :access_expired(86400)
+               :build()
+
+-----------------------------------------------------------
+--- 服务业务接口
+-----------------------------------------------------------
+local command= {}
+
+
+function command.load_data(source, type_list)
+    local u1 = cache:get(source)
+    if u1 then
+        return social.get_all_friend_data(u1, type_list)
+    end
 end
 
--- 获取好友排行数据
-function CMD.get_charts_data(uid, charts_id)
-    return friend_mgr:get_charts_data(uid, charts_id)
+-- 提交好友申请
+-- 1. 申请来源
+-- 2. 申请目标
+-- 3. 申请留言
+function command.submit_application(source, target, message)
+    local u1 = cache:get(source)
+    local u2 = cache:get(target)
+    if (u1 == nil) or (u2 == nil) then
+        return ERRCODE.COMMON_SYSTEM_ERROR
+    end
+    if source == target then
+        return ERRCODE.COMMON_PARAMS_ERROR
+    end
+    -- 检查是否好友
+    if u1:is_friend(target) then
+        return ERRCODE.SOCIAL_ALREADY_FRIEND
+    end
+    -- 检查是否申请
+    if u1:has_applied(target) then
+        return ERRCODE.SOCIAL_ALREADY_APPLIED
+    end
+    -- 检查申请上限
+    if u1:applicant_has_full() then
+        return ERRCODE.SOCIAL_APPLIED_FULL
+    end
+    if u2:authorize_has_full() then
+        return ERRCODE.SOCIAL_AUTHORIZE_FULL
+    end
+    -- 提交好友申请
+    u1:add_applicant(target)
+    u2:add_authorize(source, message)
+    return 0
 end
 
--- 判断角色是否好友关系
-function CMD.is_friend(u1, u2)
-    return friend_mgr:is_friend(u1, u2)
+-- 同意好友申请
+-- 1. 角色编号
+-- 2. 目标编号
+function command.agree_application(source, target)
+    local u1 = cache:get(source)
+    local u2 = cache:get(target)
+    if (u1 == nil) or (u2 == nil) then
+        return ERRCODE.COMMON_SYSTEM_ERROR
+    end
+    -- 检查是否申请
+    if not u2:has_applied(source) then
+        return ERRCODE.COMMON_PARAMS_ERROR
+    end
+    -- 检查好友容量
+    if u1:friend_has_full() or u2:friend_has_full() then
+        return ERRCODE.SOCIAL_FRIEND_FULL
+    end
+    -- 同意好友申请
+    u1:del_authorize(target)
+    u2:del_applicant(source)
+    u1:del_enemy(target)
+    u2:del_enemy(source)
+    u1:add_friend(target)
+    u2:add_friend(source)
+    return 0
 end
 
--- 退出处理
-local function exit_handler()
-    this.unschedule_all()
-
-    save_data()
+-- 拒绝好友申请
+-- 1. 角色编号
+-- 2. 目标编号
+function command.reject_application(source, target)
+    local u1 = cache:get(source)
+    local u2 = cache:get(target)
+    if (u1 == nil) or (u2 == nil) then
+        return ERRCODE.COMMON_SYSTEM_ERROR
+    end
+    -- 检查是否申请
+    if not u2:has_applied(source) then
+        return ERRCODE.COMMON_PARAMS_ERROR
+    end
+    -- 拒绝好友申请
+    u1:del_authorize(target)
+    u2:del_applicant(source)
+    return 0
 end
 
--- 服务注册
-
-local handler = {}
-
--- 消息分发逻辑
--- 1. 消息来源
--- 2. 消息类型
--- 3. 消息内容
-function handler.command_handler(source, cmd, ...)
-	local fn = CMD[cmd]
-	if fn then
-		return fn(...)
-	else
-		ERROR("svcmanager : command[%s] can't find!!!", cmd)
-	end
+-- 移除好友
+-- 1. 角色编号
+-- 2. 好友编号
+function command.delete_friend(source, target)
+    local u1 = cache:get(source)
+    local u2 = cache:get(target)
+    if (u1 == nil) or (u2 == nil) then
+        return ERRCODE.COMMON_SYSTEM_ERROR
+    end
+    -- 检查是否好友
+    if not u1:is_friend(target) then
+        return ERRCODE.COMMON_PARAMS_ERROR
+    end
+    -- 移除指定好友
+    u1:del_friend(target)
+    u2:del_friend(source)
+    return 0
 end
 
-handler.init_handler = init_handler
-handler.exit_handler = exit_handler
+-- 添加敌人
+-- 1. 角色编号
+-- 2. 敌人编号
+function command.append_enemy(source, target)
+    local u1 = cache:get(source)
+    local u2 = cache:get(target)
+    if (u1 == nil) or (u2 == nil) then
+        return ERRCODE.COMMON_SYSTEM_ERROR
+    end
+    if source == target then
+        return ERRCODE.COMMON_PARAMS_ERROR
+    end
+    -- 检查敌人容量
+    if u1:enemy_has_full() then
+        return ERRCODE.SOCIAL_ENEMY_FULL
+    end
+    -- 添加敌人（敌人无需双向确认）
+    if u1:is_friend(target) then
+        u1:del_friend(target)
+        u2:del_friend(source)
+    end
+    u1:add_enemy(target)
+    return 0
+end
 
-service.start(handler)
+-- 移除敌人
+-- 1. 角色编号
+-- 2. 敌人编号
+function command.remove_enemy(source, target)
+    local u1 = cache:get(source)
+    if (u1 == nil) then
+        return ERRCODE.COMMON_SYSTEM_ERROR
+    end
+    u1:del_enemy(target)
+    return 0
+end
+
+-----------------------------------------------------------
+--- 服务回调接口
+-----------------------------------------------------------
+local server = {}
+
+-- 服务启动通知
+function server.on_init()
+end
+
+-- 服务结束通知
+function server.on_exit()
+    cache:clear()
+end
+
+-- 内部指令通知
+-- 1. 指令来源
+-- 2. 指令名称
+-- 3. 执行参数
+function server.on_command(source, cmd, ...)
+    local fn = command[cmd]
+    if fn then
+        return fn(...)
+    else
+        ERROR("social : command[%s] not found!!!", cmd)
+    end
+end
+
+-- 启动服务
+service.start(server)
