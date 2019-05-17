00e8a1bf624d7a4c75c89211a66cfa96801775bf
dengcs <540390359@qq.com>
2019-05-17 17:11:17 +0800

    up

---
+++ center/lualib/handler/net_command.lua
@@ -0,0 +1,39 @@
+-----------------------------------------------------------
+--- 测试命令
+-----------------------------------------------------------
+local skynet  	= require "skynet"
+
+
+local HANDLER		= {}
+local REQUEST 		= {}
+
+-----------------------------------------------------------
+--- 内部变量/内部逻辑
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+--- 网络请求接口
+-----------------------------------------------------------
+
+function REQUEST:game_cmd_test()
+	local cmd_str = self.proto.cmd_str
+
+	local params = cmd_str:split(" ")
+	local cmd = table.remove(params, 1)
+
+	local handler = {}
+
+	function handler.test(p1, p2)
+		print("p1", p1, "p2", p2)
+	end
+
+	local fn = handler[cmd]
+	if IS_FUNCTION(fn) then
+		fn(table.unpack(params))
+	end
+
+	self.response("game_cmd_test_resp")
+end
+
+HANDLER.REQUEST   	= REQUEST
+return HANDLER
---
+++ gate/lualib/config/proto/pb/command.pb
@@ -0,0 +1,10 @@
+
+ù
+src/command.proto
+game.proto"#
+cmd_test
+cmd_str (	RcmdStr"
+cmd_test_resp"(
+game_cmd_test
+cmd_str (	RcmdStr"
+game_cmd_test_respbproto3
